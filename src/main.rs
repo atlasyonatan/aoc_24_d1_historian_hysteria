710c3ace0dc068274921909ce42d6be8f64276a2
Yonatan Atlas <atlasyonatan@gmail.com>
2024-12-09 23:00:36 +0200

    finished p2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::io::{stdin, Read};
 
+use itertools::Itertools;
+
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).expect("No input");
+
     let (mut left, mut right): (Vec<_>, Vec<_>) = input
         .lines()
         .map(|line| {
             let mut iter = line
                 .split_ascii_whitespace()
-                .map(|s| s.parse::<i32>().unwrap());
+                .map(|s| s.parse::<u32>().unwrap());
             let left = iter.next().unwrap();
             let right = iter.next().unwrap();
             (left, right)
@@ -25,4 +28,16 @@ fn main() {
         .sum();
 
     println!("part 1: {result}");
+
+    let occurance_map = right
+        .into_iter()
+        .into_grouping_map_by(|n| *n)
+        .fold(0usize, |acc, _, _| acc + 1);
+
+    let result: usize = left
+        .into_iter()
+        .filter_map(|l| occurance_map.get(&l).and_then(|&r| Some(r * l as usize))) //similarity scores
+        .sum();
+
+    println!("part 2: {result}");
 }
